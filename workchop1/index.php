bd2df6723f6dff228b6476ad3021d29f50c5b7dc
Romario <romarioarroliga@gmail.com>
2020-06-15 03:59:38 -0600

    start and end

---
+++ workchop1/index.php
@@ -0,0 +1,22 @@
+<?php
+
+
+
+$fecha= 'Fecha:            '. date('Y-m-d') ."\n";
+echo($fecha);
+
+echo strftime("Hora:           %H:%M");
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/styles.css">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
